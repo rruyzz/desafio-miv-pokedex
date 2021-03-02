4b0c4937a52811e081d2bd9a82fd88964ad2b423
rodolfo <rodolfo@xds-admins-Mac-mini.local>
2021-03-02 13:35:27 -0300

    splash screen

---
+++ app/src/main/java/com/example/pokedex/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.example.pokedex
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        scope.launch {
+            delay(2000)
+            navigate()
+        }
+    }
+
+
+    private fun navigate(){
+        val action = SplashFragmentDirections.actionSplashFragmentToMainFragment()
+        findNavController().navigate(action)
+    }
+
+}
